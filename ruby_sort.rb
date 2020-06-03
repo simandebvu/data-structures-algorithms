1953bd758e7b44962b838e58105c2a2fe6199f45
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-03 23:41:15 +0200

    Added #selection_sort

--- ruby_sort.rb
+++ ruby_sort.rb
@@ -1,6 +1,7 @@
 class Array
-  def quicksort
+  def quick_sort
     return [] if empty?
+
     pivot = delete_at(rand(size))
     left, right = partition(&pivot.method(:>))
     [*left.quicksort, pivot, *right.quicksort]
@@ -9,14 +10,25 @@ class Array
   def bubble_sort
     loop do
       still_swapping = false
-      (self.length - 1).times do |i|
-      next unless self[i] > self[i + 1]
+      (length - 1).times do |i|
+        next unless self[i] > self[i + 1]
+
         self[i], self[i + 1] = self[i + 1], self[i]
-      still_swapping = true
+        still_swapping = true
       end
       break unless still_swapping
     end
     self
   end
+
+  def selection_sort
+    (length-1).times do |i|
+      (i+1).upto(length) {|j| self[i], self[j-1] = self[j-1], self[i] if self[i]>self[j-1]}
+    end
+    self
+  end
+
+
 end
+
 
--- ruby_sort_spec.rb
+++ ruby_sort_spec.rb
@@ -3,7 +3,7 @@ require 'rspec'
 
 describe 'Testing the sorting algorithms' do
   it 'Properly sorts out an array' do
-      arr = Array.new(15) { rand(1..12) }
-      expect arr.bubble_sort.to eq(arr)
+    arr = Array.new(15) { rand(1..12) }
+    expect arr.bubble_sort.to eq(arr)
   end
 end
